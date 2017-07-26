7d8429b00414fc08222e97e8b2d2e3a2c111c3a6
Ben Goff <benegoff@gmail.com>
2017-07-26 08:48:45 -0600

    Created index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    pug = require('pug'),
+    path = require('path');
+    config = require('./config');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.listen(3000);
