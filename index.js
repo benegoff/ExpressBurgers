1b7053c4a954252b7238dd8bf7dae4f11f3bffa8
Ben Goff <benegoff@gmail.com>
2017-07-27 08:13:08 -0600

    Added config.json and made pages work with it.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     pug = require('pug'),
     path = require('path');
+    config = require('./config');
 
 var app = express();
 
@@ -8,6 +9,23 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
 
+app.get('/', function(req, res){
+    res.render('index', {
+        title: config.titles[0][1],
+        config: config
+    });
+});
 
+app.get('/:page', function(req, res){
+    var title = "";
+    for(var i = 0; i < config.titles; i++){
+        if(config.titles[i][0] === req.params.page){
+            title = config.titles[i][1];
+            
+        }
+    }
+    res.render(req.params.page, {title: title,
+                                config: config});
+});
 
 app.listen(3000);
